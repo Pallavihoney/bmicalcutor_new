b84a9e0b08f65dce01c936a9207bf075b9001697
Pallavihoney <ummadipallavi20@gmail.com>
2023-07-11 18:15:29 +0530

    updated changes

--- frontend/reactjs/my-app/src/BMIForm.js
+++ frontend/reactjs/my-app/src/BMIForm.js
@@ -1,186 +1,153 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './bmiform.css'; // Import the CSS file
 import NormalImage from './images/Normal.png';
 import ObesityImage from './images/Obesity.png';
 import UnderweightImage from './images/underweight.png';
 import OverweightImage from './images/overweight.png';
+import axios from 'axios';
 
-class BMIForm extends Component {
-  state = {
-    name: '',
-    age: '',
-    height: '',
-    weight: '',
-    mobileNumber: '',
-    errors: {},
-    bmi: null,
-    category: '',
-    image:null,
-  };
+      
+const App = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [bmi, setBMI] = useState('');
+  const [error, setError] = useState('');
+  const [image, setImage] = useState('');
 
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
+  const calculateBMI = () => {
+    const heightInMeters = height / 100; // Convert height to meters
+    const bmiResult = weight / (heightInMeters * heightInMeters);
+    setBMI(bmiResult.toFixed(2));
 
-  validateForm = () => {
-    const { name, age, height, weight, mobileNumber } = this.state;
-    const errors = {};
-
-    // Regular expression to match alphabetic characters only
-    const nameRegex = /^[A-Za-z]+$/;
-
-    // Validate name field
-    if (name.trim() === '') {
-      errors.name = 'Please enter your name.';
-    } else if (!nameRegex.test(name)) {
-      errors.name = 'Name should contain only alphabetic characters.';
-    }
-
-    // Validate age field
-    if (age.trim() === '' || isNaN(age) || parseInt(age, 10) <= 0) {
-      errors.age = 'Please enter a positive integer for age.';
-    }
-
-    // Validate height field
-    if (height.trim() === '' || isNaN(height) || parseFloat(height) <= 0) {
-      errors.height = 'Please enter a positive number for height.';
-    }
-
-    // Validate weight field
-    if (weight.trim() === '' || isNaN(weight) || parseFloat(weight) <= 0) {
-      errors.weight = 'Please enter a positive number for weight.';
-    }
-
-    // Validate mobile number field
-    const mobileNumberRegex = /^[6-9]\d{9}$/; // Indian mobile number format
-    if (!mobileNumberRegex.test(mobileNumber)) {
-      errors.mobileNumber = 'Please enter a valid Indian mobile number.';
-    }
-
-    this.setState({ errors });
-
-    return Object.keys(errors).length === 0; // Return true if no errors found
-  };
-
-  calculateBMI = () => {
-    const { height, weight } = this.state;
-
-    // Convert height to meters if in centimeters
-    const heightInMeters = parseFloat(height) / 100;
-
-    // Calculate BMI using the formula: BMI = weight (kg) / (height (m) * height (m))
-    const bmi = parseFloat(weight) / (heightInMeters * heightInMeters);
-
-    // Determine BMI category
-    
-    let category = '';
-    if (bmi < 18.5) {
-      category = 'Underweight';
-    } else if (bmi < 25) {
-      category = 'Normalweight';
-    } else if (bmi < 30) {
-      category = 'Overweight';
+    if (bmiResult >= 30) {
+      setImage(ObesityImage);
+    } else if (bmiResult >= 25) {
+      setImage(OverweightImage);
+    } else if (bmiResult >= 18.5) {
+      setImage(NormalImage);
     } else {
-      category = 'Obese';
-    }
-    let image='';
-    if (category === 'Underweight') {
-      image = <img src={UnderweightImage} alt="Underweight" />;
-    } else if (category === 'Normal') {
-      image = <img src={NormalImage} alt="Normal" />;
-    } else if (category === 'Overweight') {
-      image = <img src={OverweightImage} alt="Overweight" />;
-    } else if (category === 'Obesity')  {
-      image = <img src={ObesityImage} alt="Obesity" />;
-    }
-       this.setState({ bmi, category, image})
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (this.validateForm()) {
-      this.calculateBMI();
+      setImage(UnderweightImage);
     }
   };
 
-  render() {
-    // eslint-disable-next-line
-    const { name, age, height, weight, mobileNumber, errors, bmi, category, image=image} = this.state;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
+    if (validateForm()) {
+      calculateBMI();
+      saveUserData();
+    }
+  };
+
+  const validateForm = () => {
+    if (!name.trim() || !/^[a-zA-Z]+$/.test(name)) {
+      setError('Please enter a valid name.');
+      return false;
+    }
+    if (!age || age <= 0) {
+      setError('Please enter a valid age.');
+      return false;
+    }
+    if (!height || height <= 0) {
+      setError('Please enter a valid height.');
+      return false;
+    }
+    if (!weight || weight <= 0) {
+      setError('Please enter a valid weight.');
+      return false;
+    }
+    if (!mobileNumber || !/^[6-9]\d{9}$/.test(mobileNumber)) {
+      setError('Please enter a valid Indian mobile number.');
+      return false;
+    }
+
+    setError('');
+    return true;
+  };
+
+  const saveUserData = () => {
+    const userData = {
+      name: name,
+      age: age,
+      height: height,
+      weight: weight,
+      mobile_number: mobileNumber,
+    };
+
+    axios
+    .post('api/', userData)
+    .then((response) => {
+      console.log('User data saved successfully:', response.data);
+    })
+    .catch((error) => {
+      console.error('Error saving user data:', error);
+    });
+};
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
           <label>Name:</label>
           <input
             type="text"
-            name="name"
+            className="form-control"
             value={name}
-            onChange={this.handleInputChange}
+            onChange={(e) => setName(e.target.value)}
           />
-          {errors.name && <span className="error">{errors.name}</span>}
         </div>
-
-        <div>
+        <div className="form-group">
           <label>Age:</label>
           <input
             type="number"
-            name="age"
+            className="form-control"
             value={age}
-            onChange={this.handleInputChange}
+            onChange={(e) => setAge(e.target.value)}
           />
-          {errors.age && <span className="error">{errors.age}</span>}
         </div>
-
-        <div>
+        <div className="form-group">
           <label>Height (cm):</label>
           <input
             type="number"
-            name="height"
+            className="form-control"
             value={height}
-            onChange={this.handleInputChange}
+            onChange={(e) => setHeight(e.target.value)}
           />
-          {errors.height && <span className="error">{errors.height}</span>}
         </div>
-
-        <div>
+        <div className="form-group">
           <label>Weight (kg):</label>
           <input
             type="number"
-            name="weight"
+            className="form-control"
             value={weight}
-            onChange={this.handleInputChange}
+            onChange={(e) => setWeight(e.target.value)}
           />
-          {errors.weight && <span className="error">{errors.weight}</span>}
         </div>
-
-        <div>
+        <div className="form-group">
           <label>Mobile Number:</label>
           <input
-            type="text"
-            name="mobileNumber"
+            type="tel"
+            className="form-control"
             value={mobileNumber}
-            onChange={this.handleInputChange}
+            onChange={(e) => setMobileNumber(e.target.value)}
           />
-          {errors.mobileNumber && <span className="error">{errors.mobileNumber}</span>}
         </div>
-
-        <button type="submit">Submit</button>
-
-        {bmi && (
-          <div>
-            <h2>Results:</h2>
-            <p>BMI: {bmi.toFixed(2)}</p>
-            <p>Category: {category}</p>
-            {image}
-          </div>
-        )}
+        <button type="submit" className="btn btn-primary">Calculate BMI</button>
       </form>
-    );
-  }
-}
+      {error && <p className="error-message">{error}</p>}
+      {bmi && (
+        <div className="bmi-result">
+          <h3>BMI Result:</h3>
+           <p>{bmi}</p>
+          {image && <img src={image} alt="BMI category" />}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default BMIForm; 
+
+export default App;
